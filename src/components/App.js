0aa5d820456f80fe9a61b3206b86c1b58cadfd7f
Alex Keeley <keeley.lp@gmail.com>
2021-05-22 16:49:36 +0100

    Added basic functionality to App component which ddisplays each crud component to display the endpoint for

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => {
-  return <div>Page One</div>;
-};
-
-const PageTwo = () => {
-  return <div>Page Two</div>;
-};
-
+import StreamCreate from "../streams/StreamCreate";
+import StreamEdit from "../streams/StreamEdit";
+import StreamDelete from "../streams/StreamDelete";
+import StreamShow from "../streams/StreamShow";
+import StreamList from "../streams/StreamList";
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne}></Route>
-          <Route path="/pagetwo" exact component={PageTwo}></Route>
+          <Route path="/" exact component={StreamList}></Route>
+          <Route path="/streams/new" exact component={StreamCreate}></Route>
+          <Route path="/streams/edit" exact component={StreamEdit}></Route>
+          <Route path="/streams/delete" exact component={StreamDelete}></Route>
+          <Route path="/streams/show" exact component={StreamShow}></Route>
         </div>
       </BrowserRouter>
     </div>
