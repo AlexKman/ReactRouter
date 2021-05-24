62ba5a3b439cfb5cd133bba0fcef93158faf76f3
Alex Keeley <keeley.lp@gmail.com>
2021-05-24 19:28:23 +0100

    Sign in/out button displays dynamically whether you are signed in or not and is applied to sign you in or out vice versa

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -23,19 +23,27 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  signIn = () => {
+    this.auth.signIn();
+  };
+
+  signOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button onClick={this.signOut} className="ui red google button">
           <i className="google icon"></i>
           Sign out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button">
+        <button onClick={this.signIn} className="ui red google button">
           <i className="google icon"></i>Sign in with Google
         </button>
       );
