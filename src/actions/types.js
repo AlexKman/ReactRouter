f59bd1dd07b9183ad3e2f2328ae6662dbcc864d4
Alex Keeley <keeley.lp@gmail.com>
2021-05-29 15:43:47 +0100

    initialized redux form and imported with root reducer file

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,13 +1,2 @@
-import { SIGN_IN, SIGN_OUT } from "./types";
-
-export const signIn = () => {
-  return {
-    type: SIGN_IN,
-  };
-};
-
-export const signOut = () => {
-  return {
-    type: SIGN_OUT,
-  };
-};
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
