38fbaca7cff49a9ec88f1369cb7f9a22fb19ef45
Alex Keeley <keeley.lp@gmail.com>
2021-05-30 17:34:17 +0100

    Added button with submit functionality on the streamCreate form with basic error handling for empty values

--- src/streams/StreamCreate.js
+++ src/streams/StreamCreate.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>Stream Create</div>;
+class StreamCreate extends Component {
+  renderInput({ input, label, meta }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} autoComplete="off"></input>
+        <div>{meta.error}</div>
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field
+          name="title"
+          label="Enter Title"
+          component={this.renderInput}
+        ></Field>
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        ></Field>
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validateFormValues = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  validate: validateFormValues,
+})(StreamCreate);
