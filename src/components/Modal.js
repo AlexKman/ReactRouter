2ebd0444e7518cae04b2040b4b08d424750fdda8
Alex Keeley <keeley.lp@gmail.com>
2021-06-07 22:51:14 +0100

    Updated modal using semantic Ui to create a bigger more aesthetic component with buttons

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,14 @@ import ReactDOM from "react-dom";
 const Modal = (props) => {
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Egg</div>
+      <div className="ui standard modal visible active">
+        <div className="header">Delete stream</div>
+        <div className="content">Are you sure dude?</div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui negative button">Cancel</button>
+        </div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
