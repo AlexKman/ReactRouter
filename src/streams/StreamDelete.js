6ef6c2813451a3b0c57d0acd1292ed770bf95b6a
Alex Keeley <keeley.lp@gmail.com>
2021-06-08 13:31:25 +0100

    Refactored my modal component to be a reusable component utilizing props

--- src/streams/StreamDelete.js
+++ src/streams/StreamDelete.js
@@ -2,9 +2,21 @@ import React from "react";
 import Modal from "../components/Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      {" "}
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
   return (
     <div>
-      Stream Delete<Modal></Modal>
+      StreamDelete
+      <Modal
+        title="Delete stream"
+        content="Are you sure you want to delete?"
+        actions={actions}
+      ></Modal>
     </div>
   );
 };
